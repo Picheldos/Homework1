be10f2af883ad4c0e0c30a3305cc618f40c33b88
Picheldos <vasporelsa@gmail.com>
2021-05-12 01:34:16 +0700

    changes
    delete

--- src/components/header/header.ts
+++
@@ -1,9 +0,0 @@
-import Component, { ComponentProps } from '../../app/js/component';
-
-export default class Header extends Component.Default {
-    constructor(element: ComponentProps) {
-        super(element);
-    }
-
-    destroy = () => {};
-}
